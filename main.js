316b43722f3cc76fb7279e9d7b082b19d2840e2b
Oksana Arefeva <oksanaarefy@MacBook-Pro-Oksana.local>
2025-07-17 21:58:21 +0200

    feat:  добавлены файлы переводов locales, перевод на странице осущ-ся с помощью i18next-http-backend.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 i18next
     .use(i18nextHttpBackend)
     .init({
-    lng: 'en',
-    debug: true,
-    backend: {
-        loadPath: '/locales/{{lng}}/translation.json'
-    }
-}).then(() => {
+        lng: 'en',
+        debug: true,
+        backend: {
+            loadPath: '/locales/{{lng}}/translation.json'
+        }
+    }).then(() => {
     updateContent();
 });
 
@@ -16,24 +16,23 @@ function updateContent() {
         const key = el.getAttribute('data-i18n');
         el.innerHTML = i18next.t(key);
     });
+}
 
-    const dropdownMenu = document.querySelectorAll('.dropdown-item');
-    dropdownMenu.forEach(item => {
+document.querySelectorAll('.dropdown-item')
+    .forEach(item => {
         item.addEventListener('click', e => {
             i18next.changeLanguage(e.target.dataset.lang).then(updateContent);
         });
-    });
-}
+});
 
 const navLinks = document.querySelectorAll('.nav-link');
-
 navLinks.forEach(item => {
-        item.addEventListener('click', e => {
-            navLinks.forEach(it => {
-                it.classList.remove('active');
-            })
-            e.target.classList.add('active');
+    item.addEventListener('click', e => {
+        navLinks.forEach(it => {
+            it.classList.remove('active');
         })
+        e.target.classList.add('active');
+    })
 })
 
 
